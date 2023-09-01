7ba17a96de0e846943e12ea375864cb63261cefb
Leandro Souza <48530574+Lsouz44@users.noreply.github.com>
2023-09-01 16:59:50 -0300

    Create atvd2_2.c

---
+++ atvd2_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Vetordecrescente(char vet[], int tam) {
+    if (tam > 0) {
+        printf("%c ", vet[tam-1]);
+        Vetordecrescente(vet, tam-1);
+    }
+}
+
+int main() {
+    char vet[] = {'a', 'b', 'c', 'd', 'e'};
+    int tam = sizeof(vet) / sizeof(vet[0]);
+
+    printf("Elements do vetor em ordem descrescente: ");
+    Vetordecrescente(vet, tam);
+    printf("\n");
+
+    return 0;
+}
