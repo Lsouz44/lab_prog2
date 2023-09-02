a111620afb0382b63e0e1370128695e7ae58f284
Leandro Souza <48530574+Lsouz44@users.noreply.github.com>
2023-09-01 23:42:53 -0300

    Update atvd2_6.c

--- atvd2_6.c
+++ atvd2_6.c
@@ -22,16 +22,16 @@ Este algoritmo realiza exatamente 2^n-1 movimentos, que é provado ser o número
 #include <stdio.h>
 // --------------------------------------------
 
-void rec_hanoi (int n, char origem, char dest, char trab) {
+void rec_hanoi(int n, char origem, char dest, char trab) {
     if (n > 0) {
-        rec_hanoi (n -1, origem, trab, dest);
-        printf ("Mova o disco %d do pino %c para %c\n", n, origem, dest);
-        ec_hanoi (n -1, trab, dest, origem);
+        rec_hanoi(n -1, origem, trab, dest);
+        printf("Mova o disco %d do pino %c para %c\n", n, origem, dest);
+        rec_hanoi(n -1, trab, dest, origem);
     }
 }
 
 // MAIN
-int main () {
+int main() {
     int discos;
     printf("Digite a quantidade de discos :\n");
     scanf("%d", &discos);
